4067c555a2df1a1715c48cd6ce05340748bebec9
B.LIKHITHA <likkilikitha214@gmail.com>
2025-09-30 07:38:33 +0530

    Add files via upload

---
+++ basic_oop.py
@@ -0,0 +1,14 @@
+a=1
+print(type(a))
+a="likhitha"
+print(type(a))
+def display():
+    pass
+print(type(display))
+#for one class we can create many objects
+a="likhitha"
+print(type(a))
+class Instructor:
+    pass
+Instructor_1=Instructor()
+print(type(Instructor_1))
