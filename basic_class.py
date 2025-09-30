0e9b69a08e92a26d7d783c8c24e5e09fa70488b0
B.LIKHITHA <likkilikitha214@gmail.com>
2025-09-30 07:38:09 +0530

    Add files via upload

---
+++ basic_class.py
@@ -0,0 +1,28 @@
+class Likhitha:
+    pass
+Likhitha_1=Likhitha()
+narasimha=Likhitha()
+Rithik=Likhitha()
+print(type(Rithik))
+print(type(Likhitha_1))
+print(type(narasimha))
+
+class Car_Design:
+    pass
+Design_1=Car_Design()
+Design_2=Car_Design()
+print(type(Design_1))
+print(type(Design_2))
+
+class Likhitha:
+    pass
+Likhitha_1=Likhitha()
+Likhitha_2=Likhitha()
+Likhitha_1.name="Likki"
+Likhitha_1.address="palamaner"
+print(Likhitha_1.name)
+print(Likhitha_1.address)
+Likhitha_2.name="Rithik"
+Likhitha_2.address="banglore"
+print(Likhitha_2.name)
+print(Likhitha_2.address)
